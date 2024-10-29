f7659e7b76f5df9a6af60dcb6f482fd6f75ca116
JavierHR <javi3rhr@gmail.com>
2024-10-29 20:29:58 +0100

    first commit

---
+++ src/test/java/com/example/series_ranking/SeriesRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.series_ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeriesRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
