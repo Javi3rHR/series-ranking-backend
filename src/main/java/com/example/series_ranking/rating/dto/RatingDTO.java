eb318da9d9cdc3ab810fcf71e946d37da0bf2bcf
JavierHR <javi3rhr@gmail.com>
2024-11-04 14:53:33 +0100

    feat/ Validations

--- src/main/java/com/example/series_ranking/rating/dto/RatingDTO.java
+++ src/main/java/com/example/series_ranking/rating/dto/RatingDTO.java
@@ -1,12 +1,23 @@
 package com.example.series_ranking.rating.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RatingDTO {
 
     private Long id;
+
+    @NotNull(message = "Rating cannot be null")
+    @Min(value = 0, message = "Rating must be at least 0")
+    @Max(value = 10, message = "Rating must be at most 10")
     private Double rating;
+
+    @NotNull(message = "Series ID cannot be null")
     private Long seriesId;
+
+    @NotNull(message = "User ID cannot be null")
     private Long userId;
 }
